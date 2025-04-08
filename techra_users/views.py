5537145a53973ac6cbb5f9428107ff741551ca39
Jonathan Alfaro <jonas4784@gmail.com>
2025-04-08 14:37:34 -0600

    ADD. Se agregó el número de tickets que tiene un usuario

--- techra_users/views.py
+++ techra_users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 from .models import TechraUsers
 
+from tickets.models import Ticket
 @login_required(login_url='login')
 def techra_users_list(request):
     techra_users = TechraUsers.objects.all()
@@ -10,11 +11,13 @@ def techra_users_list(request):
     #     print("No se encontraron usuarios en la base de datos.")
     #     techra_user = TechraUsers(id_usuario=1, nombre='John Doe')
     #     techra_user.save()
+    for techra_user in techra_users:
+        techra_user.numero_tickets = Ticket.objects.filter(tecnico=techra_user.nombre).count()
     return render(request, 'techra_users.html', {'techra_users': techra_users})
 
 def detalle_techra_user(request, nombre_usuario):
     techra_user = {
-        'id_usuario': 1000,
         'nombre': nombre_usuario
     }
-    return render(request, 'detalle_techra_user.html', {'techra_user': techra_user})
+    tickets = Ticket.objects.filter(tecnico=nombre_usuario)
+    return render(request, 'detalle_techra_user.html', {'techra_user': techra_user, 'tickets': tickets})
