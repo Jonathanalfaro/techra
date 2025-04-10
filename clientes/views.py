821acee053fb0c8c15ad70306b03d098fe5fa7e5
Jonathan Alfaro <jonas4784@gmail.com>
2025-04-09 18:24:05 -0600

    ADD. Vista clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+
+from tickets.models import Ticket
 from .models import Cliente
 # Create your views here.
 
 def lista_clientes(request):
     clientes = Cliente.objects.all()
+    # if not clientes:
+    #     tickets = Ticket.objects.all()
+    #     for ticket in tickets:
+    #         cliente = Cliente(nombre=ticket.cliente, clave_cliente=ticket.clave_cliente)
+    #         if not Cliente.objects.filter(clave_cliente=cliente.clave_cliente).exists():
+    #             cliente.save()
+    #     clientes = Cliente.objects.all()
     return render(request, 'clientes.html',{'clientes':clientes})
 
 
 def detalle_cliente(request, cliente):
     cliente = Cliente.objects.filter(clave_cliente=cliente)
-    return render(request, 'detalle_cliente.html', {'cliente': cliente})
+    tickets = Ticket.objects.filter(clave_cliente=cliente[0].clave_cliente)
+    return render(request, 'detalle_cliente.html', {'cliente': cliente[0], 'tickets':tickets})
